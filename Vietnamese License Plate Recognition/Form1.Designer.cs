bea02c30ea659bd626af2ec5988db05a58e37d54
nguyennt <nguyennt@tandan.com.vn>
2022-07-08 13:53:28 +0700

    int

--- Vietnamese License Plate Recognition/Form1.Designer.cs
+++ Vietnamese License Plate Recognition/Form1.Designer.cs
@@ -30,24 +30,25 @@
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.imageBox1 = new Emgu.CV.UI.ImageBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -66,6 +67,44 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Quá trình nhận dạng";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(290, 255);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(219, 156);
+            this.panel1.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(9, 255);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(227, 156);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(290, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(219, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(9, 61);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(227, 156);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
             // 
             // label4
             // 
@@ -105,62 +144,24 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.pictureBox3);
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Location = new System.Drawing.Point(540, 33);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(232, 483);
+            this.groupBox2.Size = new System.Drawing.Size(292, 483);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Kết quả";
             // 
-            // imageBox1
+            // pictureBox3
             // 
-            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.imageBox1.Location = new System.Drawing.Point(9, 61);
-            this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(227, 156);
-            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.imageBox1.TabIndex = 2;
-            this.imageBox1.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(290, 61);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(219, 156);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(9, 255);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(227, 156);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 5;
-            this.pictureBox2.TabStop = false;
-            // 
-            // panel1
-            // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Location = new System.Drawing.Point(290, 255);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(219, 156);
-            this.panel1.TabIndex = 6;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(68, 54);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 84);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(6, 210);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(286, 225);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
             // 
             // button1
             // 
@@ -172,30 +173,32 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
+            // textBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(6, 186);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(215, 205);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.textBox1.Location = new System.Drawing.Point(68, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 84);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(796, 458);
+            this.ClientSize = new System.Drawing.Size(858, 506);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -210,12 +213,12 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private Emgu.CV.UI.ImageBox imageBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Vietnamese License Plate Recognition/Form1.cs
+++ Vietnamese License Plate Recognition/Form1.cs
@@ -11,17 +11,19 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using Emgu.CV.CvEnum;
-
+using Emgu.CV.Util;
+using Tesseract;
 namespace Vietnamese_License_Plate_Recognition
 {
     public partial class Form1 : Form
     {
         List<Image<Bgr, Byte>> PlateImagesList = new List<Image<Bgr, byte>>();
+        List<Rectangle> listRect = new List<Rectangle>();
         public Form1()
         {
             InitializeComponent();
         }
-
+        const int Maxtrix = 3;//3x3
         private void button1_Click(object sender, EventArgs e)
         {
             // open file dialog   
@@ -45,16 +47,16 @@ namespace Vietnamese_License_Plate_Recognition
             Bitmap image = new Bitmap(startupPath);
             FindLicensePlate(image, pictureBox1, imageBox1, PlateImagesList, panel1);
         }
-
-        private void FindLicensePlate(Bitmap image, PictureBox pictureBox1, ImageBox imageBox1, List<Image<Bgr, byte>> plateImagesList, Panel panel1)
+        //Hàm xử lý biển số xe 
+        public void FindLicensePlate(Bitmap image, PictureBox pictureBox1, ImageBox imageBox1, List<Image<Bgr, byte>> plateImagesList, Panel panel1)
         {
             try
             {
+                //Detect biển số xe
                 Image<Bgr, byte> image2 = new Image<Bgr, byte>(image);
                 CascadeClassifier classifier = new CascadeClassifier(Application.StartupPath + "\\output-hv-33-x25.xml");
                 var imgGray = image2.Convert<Gray, byte>().Clone();
                 Rectangle[] plates = classifier.DetectMultiScale(imgGray, 1.1, 4);
-                //richTextBox1.Text = plates[0].ToString();
                 if (plates.Length == 0)
                 {
                     MessageBox.Show("Không thể nhận dạng được biển số xe này !", "TD SJC");
@@ -63,21 +65,96 @@ namespace Vietnamese_License_Plate_Recognition
 
                 foreach (Rectangle plate in plates)
                 {
-                    image2.Draw(plate, new Bgr(0, 255, 0), 2);
+                    if (plate.Width > 30 && plate.Width < 250 && plate.Height >10 && plate.Height < 250)
+                    {
+                        image2.Draw(plate, new Bgr(0, 255, 0), 2);
+                        imageBox1.Image = image2;
+                        Image<Bgr, byte> image3 = new Image<Bgr, byte>(image);
+                        image3.ROI = plate;
+                        pictureBox1.Image = image3.Bitmap;
+                        Console.WriteLine("W: {0}, H: {1}", plate.Width, plate.Height);
+                    }      
+                    else
+                    {
+                        MessageBox.Show("Không nhận diện được. Thử lại ảnh khác !", "TD SJC");
+                        return;
+                    }    
                 }
-                imageBox1.Image = image2;
-                int x = plates[0].X;
-                int y = plates[0].Y;
-                int w = plates[0].Width;
-                int h = plates[0].Height;
-                Image<Bgr, byte> image3 = new Image<Bgr, byte>(image);
-                image3.ROI = new Rectangle(x, y, w, h);
-                pictureBox1.Image = image3.Bitmap;
+                //Tìm Contours cho biển số vừa detect được
+                Image<Bgr, byte> src = new Image<Bgr, byte>(new Bitmap(pictureBox1.Image));
+                IdentifyContours(src, out listRect);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
         }
+        public static string OCR(Bitmap image)
+        {
+            string ocrtext = "";
+            using (var engine = new TesseractEngine(@"./data", "eng", EngineMode.Default))
+            {
+                using (var img = PixConverter.ToPix(image))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        ocrtext = page.GetText();
+                    }
+                }
+            }
+            return ocrtext;
+            //var ocr = new TesseractEngine(@"./data", "eng", EngineMode.Default);
+            //var page = ocr.Process(image);
+            //return page.GetText();
+        }
+        public void IdentifyContours(Image<Bgr,byte> colorImage, out List<Rectangle> listRect)
+        {
+            List<Rectangle> listContour = new List<Rectangle>();        
+            Image<Gray, byte> srcGray = colorImage.Convert<Gray, byte>();
+            Image<Gray, byte> imageT = new Image<Gray, byte>(srcGray.Width, srcGray.Height);
+            CvInvoke.AdaptiveThreshold(srcGray, imageT, 255.0, AdaptiveThresholdType.MeanC, ThresholdType.Binary, 45, 2.0);
+            pictureBox3.Image = imageT.Bitmap;
+            Image<Gray, byte> imageThresh = imageT;
+            imageT = imageT.ThresholdBinary(new Gray(100), new Gray(255.0));//Cần xử lý thêm để nhận dạng tốt hơn
+            VectorOfVectorOfPoint contour = new VectorOfVectorOfPoint();
+            Mat hier = new Mat();
+            CvInvoke.FindContours(imageT, contour, hier, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+            Dictionary<int, double> dict = new Dictionary<int, double>();
+            //Console.WriteLine(contour.Size);
+            if (contour.Size > 0)
+            {
+                string textPlate = "";       
+                for (int i = 0; i < contour.Size; i++)
+                {         
+                    Rectangle rect = CvInvoke.BoundingRectangle(contour[i]);                   
+                    double area = CvInvoke.ContourArea(contour[i]);
+                    double rate = (double)rect.Width / (double)rect.Height;
+                    if (rect.Width > 5 && rect.Width <50 && rect.Height >= 15 && rect.Height < 50 && rate > 0.2 && rate < 1)
+                    {
+                        //Console.WriteLine("W: {0}, H: {1}, Rate W/H: {2}, Area: {3}", rect.Width, rect.Height, rate, area);
+                        CvInvoke.Rectangle(colorImage, rect, new MCvScalar(255, 0, 0),1);
+                        Image<Gray, byte> imageCrop = imageThresh;
+                        imageCrop.ROI = rect;
+                        Image<Gray, byte> resizedImage = imageCrop.Resize(50, 90, Inter.Linear);                       
+                        Mat imgCropResize = resizedImage.Mat;               
+                        CvInvoke.Imshow("Numbers Plate", imgCropResize);
+                        textPlate += OCR(resizedImage.Bitmap);
+                        Console.Write(textPlate);
+                        //CvInvoke.Imwrite("Numbers Plate.png", imgCropResize);
+                        //Image<Gray, Byte> imgFinal = new Image<Gray, Byte>("Numbers Plate.png");
+                        CvInvoke.WaitKey(0);
+                        dict.Add(i, area);                      
+                    }                  
+                }
+                //textPlate = GetText(new Bitmap(pictureBox3.Image));
+                //MessageBox.Show(textPlate);
+            }   
+            var item = dict.OrderByDescending(v => v.Value);  
+            pictureBox2.Image = colorImage.Bitmap;
+            //textBox1.Text = item.ElementAt(1).Value.ToString(); 
+            //return out
+            listRect = null;
+        }
+        
     }
 }
