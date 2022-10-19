d7306ead9293fb8a1a8dfde31faf43a0f78c1c80
sangnv <sangnv@tandan.com.vn>
2022-10-19 10:55:33 +0700

    update limit size

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ from process import ReturnInfoLP, MessageInfo, ExtractLP
 import os
 import time
 import shutil
+
+from starlette import status
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+class LimitUploadSize(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp, max_upload_size: int) -> None:
+        super().__init__(app)
+        self.max_upload_size = max_upload_size
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        if request.method == 'POST':
+            if 'content-length' not in request.headers:
+                return Response(status_code=status.HTTP_411_LENGTH_REQUIRED)
+            content_length = int(request.headers['content-length'])
+            if content_length > self.max_upload_size:
+                return Response(status_code=status.HTTP_431_REQUEST_HEADER_FIELDS_TOO_LARGE)
+        return await call_next(request)
 app = FastAPI()
-
-
+app.add_middleware(LimitUploadSize, max_upload_size=3000000)  # ~3MB
 @app.get("/")
 def read_root():
     return {"Hello": "Tan Dan JSC"}
